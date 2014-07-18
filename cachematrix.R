2bd6f747f416dede6ac1ab04585984dc8b0f73e1
Matthew Purver <matt@chatterbox.co>
2014-07-18 15:45:01 +0100

    complete code for assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## when passed a matrix argument, return a list object to store the original matrix x and its inverse i;
+## with get, set, getinv, setinv methods (get/set for original matrix x, getinv/setinv for inverse i)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+	    if(is.matrix(y)) {
+		if( dim(x) == dim(y) && all(x == y) ) {
+		   message("identical argument, leaving cache unchanged")
+		} else {
+		   message("new argument, resetting cache")
+                   i <<- NULL
+		}
+		x <<- y
+	    } else {
+		stop("invalid argument")
+	    }
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## when passed an argument which is a makeCacheMatrix() object, get its inverse, calculating it and setting it if necessary
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
